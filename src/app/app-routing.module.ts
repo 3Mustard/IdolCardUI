5a3b8dc63ac28c028882aa8dfc810a075b4b2c42
Matthew Cullen <m23.cullen@gmail.com>
2021-03-15 10:51:52 -0400

    added some routing and fixed an error with node_modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IdolCardComponent } from './idol-card/idol-card.component';
+import { IdolGroupComponent } from './idol-group/idol-group.component';
+
+const routes: Routes = [
+  {path:'idolCard', component:IdolCardComponent},
+  {path:'idolGroup',component:IdolGroupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
