baabcb53836d91ea8c3ff162c0c833c7a1a37621
Matthew Cullen <m23.cullen@gmail.com>
2021-03-17 19:30:05 -0400

    added logic to show-card-component

--- src/app/IdolCard/show-card/show-card.component.ts
+++ src/app/IdolCard/show-card/show-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-card',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  CardList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditCardComp: boolean = false;
+  card:any;
 
   ngOnInit(): void {
+    this.refreshCardList();
+  }
+
+  addClick(): void {
+    this.card= {
+      IdolId: 0,
+      IdolName: "",
+      IdolGroup: "",
+      PhotoCardSet: "",
+      DateAdded: "",
+      PhotoFileName: "anon.png"
+    }
+    this.ModalTitle = "Add a New Card to the Collection";
+    this.ActivateAddEditCardComp = true;
+  }
+
+  editClick(cardInfo): void {
+    this.card = cardInfo;
+    this.ModalTitle = "Edit Idol Card " + this.card.IdolName;
+    this.ActivateAddEditCardComp = true;
+  }
+
+  deleteClick(cardId) {
+    if(confirm('Confirm Delete')) {
+      this.service.deleteIdolCard(cardId).subscribe(data => {
+        alert(data.toString());
+        this.refreshCardList();
+      });
+    }
+  }
+
+  closeClick(): void {
+    this.ActivateAddEditCardComp = false;
+    this.refreshCardList();
+  }
+
+  refreshCardList(): void {
+    this.service.getIdolCardList().subscribe(data => {
+      this.CardList = data;
+    });
   }
 
 }
